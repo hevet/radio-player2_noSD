c51017edc5ce7ce13df14cb0e84101bb4604b18d
Jacek <43666290+hevet@users.noreply.github.com>
2025-06-08 13:33:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6219,9 +6219,9 @@ void loop()
           if (toneSelect == 2) {toneMidValue++;}
           if (toneSelect == 3) {toneLowValue++;}
           
-          if (toneHiValue > 12) {toneHiValue = 6;}
-          if (toneMidValue > 12) {toneMidValue = 6;}
-          if (toneLowValue > 12) {toneLowValue = 6;}
+          if (toneHiValue > 12) {toneHiValue = 12;}
+          if (toneMidValue > 12) {toneMidValue = 12;}
+          if (toneLowValue > 12) {toneLowValue = 12;}
           displayEqualizer();
         }     
         else
